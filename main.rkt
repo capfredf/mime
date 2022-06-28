33ac6ea0ae1062921c3f976b3b4a7c794f9b95f3
Fred Fu <moonsolo@gmail.com>
2022-06-27 22:10:18 -0400

    ttt

--- main.rkt
+++ main.rkt
@@ -1,38 +1,57 @@
 #lang racket/base
 
-(require racket/match
-         racket/format
+(require racket/format
          rackunit
-         syntax/parse)
+         racket/match
+         (rename-in syntax/parse
+                    [nat sp:nat]))
 
-(define (lookup-env env var)
-  (cdr (assoc var env free-identifier=?)))
+(module typed-me typed/racket/base
+  (require racket/match)
 
-(define (new-env)
-  (list (cons #'add1 (make-arrow Nat Nat))))
+  (define-type Env (Listof (Pairof Identifier Type)))
 
-(define (extend-env env var ty)
-  (cons (cons var ty) env))
+  (provide (all-defined-out))
 
-(struct type () #:transparent)
-(struct arrow type (arg-type ret-type) #:transparent #:constructor-name make-arrow)
-(struct nat type () #:transparent #:constructor-name make-nat)
-(define Nat (make-nat))
+  (: lookup-env (-> Env Identifier Type))
+  (define (lookup-env env var)
+    (cond
+      [(assoc var env free-identifier=?)
+       =>
+       cdr]
+      [else (error (format "~a is unbound" var))]))
 
-(define (type-eq? ty1 ty2)
-  (equal? ty1 ty2))
+  (: new-env (-> Env))
+  (define (new-env)
+    (list (cons #'add1 (make-arrow Nat Nat))))
 
-(define (base-type a)
-  (match (syntax-e a)
-    ['Nat Nat]
-    [_ #f]))
+  (: extend-env (-> Env Identifier Type Env))
+  (define (extend-env env var ty)
+    (cons (cons var ty) env))
+
+  (struct type () #:transparent #:type-name Type)
+  (struct arrow type ([arg-type : Type] [ret-type : Type]) #:transparent #:constructor-name make-arrow)
+  (struct nat type () #:transparent #:constructor-name make-nat)
+  (define Nat (make-nat))
+
+  (define (type-eq? ty1 ty2)
+    (equal? ty1 ty2))
+
+  (: base-type (-> Symbol (Option Type)))
+  (define (base-type a)
+    (match a
+      ['Nat Nat]
+      [_ #f])))
+
+(require 'typed-me)
 
 (define (parse-type ty-stx)
   (syntax-parse ty-stx
     #:datum-literals (->)
     [identifier:id
-     #:when (base-type #'identifier)
-     (base-type #'identifier)]
+     #:do [(define rr (base-type (syntax-e #'identifier)))]
+     #:when rr
+     rr]
     [(-> arg-type ret-type)
      (make-arrow (parse-type #'arg-type)
                  (parse-type #'ret-type))]))
