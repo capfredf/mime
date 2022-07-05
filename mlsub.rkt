dd57f7f2fcfbb18797dd91ab1fd8790ff3191b1e
Fred Fu <moonsolo@gmail.com>
2022-07-05 11:57:04 -0400

    update

--- mlsub.rkt
+++ mlsub.rkt
@@ -26,7 +26,10 @@
 
 
   (struct variable-state ([lbs : (Listof MonoType)]
-                          [ubs : (Listof MonoType)]) #:type-name VariableState #:transparent)
+                          [ubs : (Listof MonoType)])
+    #:type-name VariableState
+    #:transparent
+    #:mutable)
   (struct var ([state : VariableState]) #:type-name Var #:transparent)
   (struct arrow ([arg-type : MonoType] [ret-type : MonoType]) #:type-name Arrow #:transparent)
   (struct prim ([name : Symbol]) #:type-name Prim #:transparent)
@@ -34,28 +37,86 @@
 
 
   (define (fresh-var! [debug-symbol : Symbol]) : Var
-    (var (variable-state null null))))
+    (var (variable-state null null)))
+
+  (define (constrain! [lhs : MonoType] [rhs : MonoType]) : Void
+    (void)))
 
 
 (module infer racket/base
   (require syntax/parse
            racket/match
+           (for-syntax racket/base)
            (submod ".." typed-me))
   (provide (all-defined-out))
+  (define-syntax (rcd stx)
+    (error 'hi "don't call me"))
+
+  (define-syntax (sel stx)
+    (error 'hi "don't call me"))
+
   (define (type-infer term env)
     (syntax-parse term
-      #:literals (lambda let)
+      #:literals (lambda let rcd sel)
       [var:id
        (lookup-env env #'var)]
       [var:nat
-       (prim 'nat)])))
+       (prim 'nat)]
+      [(rcd (f:id e:expr) ...)
+       (record (map (lambda (n e)
+                      (cons (syntax-e n) (type-infer e env)))
+                    (syntax->list #'(f ...))
+                    (syntax->list #'(e ...))))]
+      [(lambda (x:id) body:expr)
+       (define ty^ (fresh-var! 'alpha))
+       (arrow ty^ (type-infer #'body
+                              (extend-env env #'x ty^)))]
+      [(f arg)
+       (define ty (fresh-var!))
+       (constrain! (type-infer #'f env)
+                   (arrow (type-infer #'arg env) ty))
+       ty]
+      [(sel rcd name)
+       (define ty (fresh-var!))
+       (constrain! (type-infer #'rcd env)
+                   (record (list (cons (syntax-e #'name) ty))))
+       ty])))
 
 
 (module+ test
-  (require rackunit)
+  (require rackunit
+           racket/match)
   (require (submod ".." infer)
            (submod ".." typed-me))
+
+  (define (alpha-eq? t1 t2)
+    (match* (t1 t2)
+      [((? var?) (? var?)) #t]
+      #;
+      [((? poly-type?) (? poly-type?))
+       (alpha-eq? (poly-type-body t1)
+                  (poly-type-body t2))]
+      [((? arrow?) (? arrow?))
+       (and (alpha-eq? (arrow-arg-type t1)
+                       (arrow-arg-type t2))
+            (alpha-eq? (arrow-ret-type t1)
+                       (arrow-ret-type t2)))]
+      [((struct prim [a]) (struct prim [b]))
+       (and (equal? a b) a b)]))
+
   (define-syntax-rule (tc given expected)
     (check-equal? (type-infer given (new-env))
                   expected))
-  (tc 10 (prim 'nat)))
+
+  (define-syntax-rule (tc-alpha given expected)
+    (check-true (alpha-eq? (type-infer given (new-env))
+                           expected)))
+  (tc #'10 (prim 'nat))
+  (tc #'(rcd [a 10]) (record (list [cons 'a (prim 'nat)])))
+  (tc-alpha #'(lambda (a) a)
+            (let ([v (fresh-var! 'a)])
+              (arrow v v)))
+  #;
+  (tc-alpha #'(lambda (a) (sel a x))
+            (let ([v (fresh-var! 'a)])
+              (arrow v v))))
